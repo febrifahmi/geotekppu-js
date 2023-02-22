b8266ed3e8936bf8c4621a0ccb207f5760fb3993
Febri Fahmi <fahmi_fafa@yahoo.com>
2023-02-23 05:47:08 +0700

    update: adding full support for RMRb calculation/classification

--- tests/rmrb.test.js
+++ tests/rmrb.test.js
@@ -1,6 +1,18 @@
-const { CalcRMRucs } = require("../geotekppu-js/rmr/rmr_b_geocontrol2012")
+const { CalcRMRucs, CalcRMRrqdSpacing, RMRb } = require("../geotekppu-js/rmr/rmr_b_geocontrol2012")
+const { CalcDiscontinuityClass, CalcR5 } = require("../geotekppu-js/rmr/rmr__bieniawski1989")
+
 
 // Test function CalcRMRucs
 test('Test function CalcRMRucs #1', () => {
     expect(CalcRMRucs(2501)).toBe(15);
+})
+
+// Test function CalcRMRrqdSpacing
+test('Test function CalcRMRrqdSpacing #1', () => {
+    expect(CalcRMRrqdSpacing(5)).toBe(27);
+})
+
+// Test function RMRb
+test('Test function RMRb', () => {
+    expect(RMRb(CalcRMRucs(2501), CalcRMRrqdSpacing(5), CalcDiscontinuityClass(0.5, 0.1, "rough", "hl<5", "decomposed"), CalcR5("None", 0, "dry"))).toBe(76);
 })
