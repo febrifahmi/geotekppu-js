51fced53c62857a9dfbc8af3c2ea1e9a75cbaf63
Febri Fahmi <fahmi_fafa@yahoo.com>
2023-02-23 11:07:58 +0700

    update: adding full support for RMR14 calculation: RMR with three adjustment factors calculation

---
+++ geotekppu-js/rmr/rmr_celada_etal2014.js
@@ -0,0 +1,140 @@
+/*
+Copyright (2023) Febri Fahmi Hakim (fahmi_fafa@yahoo.com) and Daru Jaka Sasangka (darujakasasangka@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+/**
+ * F0 is adjustment factor for the orientation of tunnel axis with regard to main set of discontinuities.
+ * @param {String} strike_orientation orientation of strike to tunnel axis ('dwd' or drive with dip, 'dad' or drive against dip, 'parallel', 'irrespective')
+ * @param {Number} dip_angle dip angle (dwd, dad, parallel: 45-90 or 20-45, irrespective: 0-20)
+ * @returns {Number}
+ */
+function CalcF0(strike_orientation, dip_angle){
+    let val_f0 = 0
+    if(strike_orientation == "dwd"){
+        if(dip_angle >= 20 && dip_angle < 45){
+            val_f0 = -2
+        } else if(dip_angle >= 45 && dip_angle <=90){
+            val_f0 = 0
+        } else if(dip_angle < 20){
+            val_f0 = null
+        } else if(dip_angle > 90){
+            val_f0 = null
+        }
+    } else if(strike_orientation == "dad"){
+        if(dip_angle >= 20 && dip_angle < 45){
+            val_f0 = -10
+        } else if(dip_angle >= 45 && dip_angle <=90){
+            val_f0 = -5
+        } else if(dip_angle < 20){
+            val_f0 = null
+        } else if(dip_angle > 90){
+            val_f0 = null
+        }
+    } else if(strike_orientation == "parallel"){
+        if(dip_angle >= 20 && dip_angle < 45){
+            val_f0 = -5
+        } else if(dip_angle >= 45 && dip_angle <=90){
+            val_f0 = -12
+        } else if(dip_angle < 20){
+            val_f0 = null
+        } else if(dip_angle > 90){
+            val_f0 = null
+        }
+    } else if(strike_orientation == "irrespective"){
+        if(dip_angle >= 0 && dip_angle <= 20){
+            val_f0 = -5
+        } else if(dip_angle > 20){
+            val_f0 = null
+        }
+    }
+    return val_f0
+}
+
+
+/**
+ * Adjusment factor for RMR considering excavation method (Tunneling Bore Method/TBM or Drill and Blast/D+B).
+ * @param {Number} rmrb RMRb rating value before adjustments (for rmrb > 40 and rmrb < 40)
+ * @returns {Number}
+ */
+function CalcFexcavation(rmrb){
+    let val_fe = 0
+    if(rmrb < 40 && rmrb >= 0){
+        val_fe = 1 + (2 * (rmrb / 100)**2)
+    } else if(rmrb >= 40 && rmrb <= 100){
+        val_fe = 1.32 - (Math.sqrt(rmrb-40)/25)
+    }
+    return val_fe
+}
+
+
+/**
+ * "Índice de Comportamiento Elástico" (ICE) as proposed by Bieniawski and Celada (2011).
+ * @param {Number} rmrb value of RMRb
+ * @param {Number} ucs uniaxial compressive strength of intact rock (in MPa)
+ * @param {Number} k0 ratio of the horizontal to vertical virgin stress
+ * @param {Number} H tunnel depth (in meter)
+ * @param {Number} F shape coefficient (circular tunnel d = 6 m -> F 1.3 ; circular tunnel d = 10 m -> F 1.0 ; coventional tunnel 14 m wide -> F 0.75 ; caverns 25 m wide x 60 m high -> F 0.55)
+ * @returns {Number}
+ */
+function CalcICE(rmrb,ucs,k0,H,F){
+    let val_ice = 0
+    const e = 2.71828183
+    if(k0 <= 1){
+        val_ice = ((3704*ucs*(e**((rmrb-100)/24)))/((3-k0)*H))*F
+    } else if(k0 >1){
+        val_ice = ((3704*ucs*(e**((rmrb-100)/24)))/(((3*k0)-1)*H))*F
+    }
+    return +val_ice.toFixed(4) // since toFixed() will output/change the value of a float with 4 decimal digits to String, we put + plus sign in front of val_ice to keep its data type as a Number
+}
+
+
+/**
+ * Adjustment factor of stress-strain based on "Índice de Comportamiento Elástico" (ICE) value.
+ * @param {Number} ice "Índice de Comportamiento Elástico" (ICE) value.
+ * @returns {Number}
+ */
+function CalcFStressStrain(ice){
+    let val_fs = 0
+    if(ice < 15){
+        val_fs = 1.3
+    } else if(ice >= 15 && ice < 70){
+        val_fs = (2.3*Math.sqrt((100-ice)))/(7.1+Math.sqrt((100-ice)))
+    } else if(ice >= 70){
+        val_fs = 1
+    }
+    return +val_fs.toFixed(4) // since toFixed() will output/change the value of a float with 4 decimal digits to String, we put + plus sign in front of val_ice to keep its data type as a Number
+}
+
+
+/**
+ * RMRb adjusted with F0 value.
+ * @param {Number} rmrb RMRb value
+ * @param {Number} f0 F0 value
+ * @returns {Number}
+ */
+function RMRbAdj(rmrb,f0){
+    let rmradj = rmrb + f0
+    return rmradj
+}
+
+
+/**
+ * RMR14 as proposed by Celada etal (2014) - RMR with three adjusment factors applied.
+ * @param {*} rmrb_adj RMRb adjustment factor fot tunnel orientation 
+ * @param {*} val_fe Fe adjustment factor for excavation
+ * @param {*} val_fs Fs adjustment factor for stress strain
+ * @returns 
+ */
+function RMR14(rmrb_adj, val_fe, val_fs){
+    let rmr14 = rmrb_adj * val_fe * val_fs
+    return +rmr14.toFixed(2) // since toFixed() will output/change the value of a float with 4 decimal digits to String, we put + plus sign in front of val_ice to keep its data type as a Number
+}
+
+module.exports = { CalcF0, CalcFexcavation, CalcICE, CalcFStressStrain, RMRbAdj, RMR14 }
---
+++ tests/rmr14.test.js
@@ -0,0 +1,60 @@
+const { CalcF0, CalcFexcavation, CalcICE, CalcFStressStrain, RMRbAdj, RMR14 } = require("../geotekppu-js/rmr/rmr_celada_etal2014")
+
+// Test function CalcF0
+test('Test function CalcF0 #1', () => {
+    expect(CalcF0("dwd",30)).toBe(-2);
+})
+
+test('Test function CalcF0 #2', () => {
+    expect(CalcF0("dad",91)).toBe(null);
+})
+
+test('Test function CalcF0 #3', () => {
+    expect(CalcF0("parallel",91)).toBe(null);
+})
+
+test('Test function CalcF0 #4', () => {
+    expect(CalcF0("irrespective",91)).toBe(null);
+})
+
+
+// Test function CalcFexcavation
+test('Test function CalcFexcavation #1', () => {
+    expect(CalcFexcavation(39)).toBe(1.3042);
+})
+
+test('Test function CalcFexcavation #2', () => {
+    expect(CalcFexcavation(41)).toBe(1.28);
+})
+
+test('Test function CalcFexcavation #3', () => {
+    expect(CalcFexcavation(40)).toBe(1.32);
+})
+
+// Testing function CalcICE
+test('Testing function CalcICE #1', () => {
+    expect(CalcICE(50,251,2,20,1.3)).toBe(1504.9008);
+})
+
+// Testing function CalcFStressStrain
+test('Testing function CalcFStressStrain #1', () => {
+    expect(CalcFStressStrain(1504.9008)).toBe(1);
+})
+
+test('Testing function CalcFStressStrain #2', () => {
+    expect(CalcFStressStrain(14)).toBe(1.3);
+})
+
+test('Testing function CalcFStressStrain #3', () => {
+    expect(CalcFStressStrain(17)).toBe(1.2926);
+})
+
+// Testing function RMRbAdj
+test('Testing function RMRbAdj', () => {
+    expect(RMRbAdj(50,-5)).toBe(45);
+})
+
+// Testing function RMR14
+test('Testing function RMR14', () => {
+    expect(RMR14(45,1.32,1.3)).toBe(77.22);
+})
